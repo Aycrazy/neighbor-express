c97eed386be32a067e90934e6c6898c458205c8d
Andrew Yaspan <andrewyaspan@Andrews-MacBook-Air-2.local>
2020-09-07 12:23:18 -0500

    exploiting foilum/branca objects

--- python/techuity.py
+++ python/techuity.py
@@ -13,6 +13,7 @@ import numpy as np
 from shapely.geometry import LineString, MultiLineString
 from plotly.express import choropleth_mapbox
 import folium
+import branca
 #%%
 
 int_df = pd.read_csv('internet_clean.csv')
@@ -132,27 +133,116 @@ fig_int
 # %%
 
 #% internet access by household folium 
+style_function = lambda x: {'fillColor': '#ffffff', 
+                            'color':'#000000', 
+                            'fillOpacity': 0.1, 
+                            'weight': 0.1}
 
 m1 = folium.Map(
     location=milwaukee_coords,
     zoom_start=10
 )
 
-folium.Choropleth(
+int_layer  = folium.Choropleth(
     geo_data=zips,
-    name='choropleth',
+    name='Internet (General)',
     data=int_x_race_df,
     columns=['ZIPCODE','percent_int'],
     key_on='feature.properties.ZIPCODE',
     fill_color='YlGn',
     fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name='Internet Access (%)').add_to(m1)
+    line_opacity=0.2
+    ,highlight=True
+    #legend_name='Internet Access (%)')#.add_to(m1) 
+    )
 
+bb_layer = folium.Choropleth(
+    geo_data=zips,
+    name='Broadband',
+    data=int_x_race_df,
+    columns=['ZIPCODE','percent_bb'],
+    key_on='feature.properties.ZIPCODE',
+    fill_color='YlGn',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    highlight=True,
+    show=False
+    #,legend_name='Broadband (%)'
+    )#.add_to(m1)
+
+
+style_function = lambda x: {'fillColor': '#ffffff', 
+                            'color':'#000000', 
+                            'fillOpacity': 0.1, 
+                            'weight': 0.1}
+                            
+int_layer_tt = folium.features.GeoJson(
+    zips,
+    style_function=style_function, 
+    control=False,
+    
+tooltip=folium.features.GeoJsonTooltip(
+        fields=['ZIPCODE'],
+        aliases=['Zipcode: '],
+        style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"))
+
+    )    
+#int_layer.add_to(m1)
+
+#bb_layer.add_to(m1)
+#Workaround
+int_layer.__dict__['_children'].pop(list(int_layer.__dict__['_children'].keys())[1])
+
+m1.add_child(int_layer)
+m1.add_child(bb_layer)
+m1.add_child(int_layer_tt)
+m1.keep_in_front(int_layer_tt)
 folium.LayerControl().add_to(m1)
 
 m1
+# %%
+import collections
+
 
 # %%
 
+m1.save('internet_access_fol.html')
 #
+
+# %%
+
+
+
+# import branca
+# import folium
+
+# from branca.element import MacroElement
+
+# from jinja2 import Template
+
+# class BindColormap(MacroElement):
+#     """Binds a colormap to a given layer.
+
+#     Parameters
+#     ----------
+#     colormap : branca.colormap.ColorMap
+#         The colormap to bind.
+#     """
+#     def __init__(self, layer, colormap):
+#         super(BindColormap, self).__init__()
+#         self.layer = layer
+#         self.colormap = colormap
+#         self._template = Template(u"""
+#         {% macro script(this, kwargs) %}
+#             {{this.colormap.get_name()}}.svg[0][0].style.display = 'block';
+#             {{this._parent.get_name()}}.on('overlayadd', function (eventLayer) {
+#                 if (eventLayer.layer == {{this.layer.get_name()}}) {
+#                     {{this.colormap.get_name()}}.svg[0][0].style.display = 'block';
+#                 }});
+#             {{this._parent.get_name()}}.on('overlayremove', function (eventLayer) {
+#                 if (eventLayer.layer == {{this.layer.get_name()}}) {
+#                     {{this.colormap.get_name()}}.svg[0][0].style.display = 'none';
+#                 }});
+#         {% endmacro %}
+#         """)  # noqa
+
